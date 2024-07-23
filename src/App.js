f421b6a18ccf16a24cdda5c25cef1f050d5efa58
Vikas <7014.vikas@gmail.com>
2024-07-23 18:25:12 +0530

    Setteld all path for next step

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 // import React from "react";
-
+import { AllRoutes} from "./routes/AllRoutes";
 export const App = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <AllRoutes  />
     </div> 
   );
 };
