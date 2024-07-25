da399435bb4fbfb1aad3acde6dfecf9775aac3bb
Vikas <7014.vikas@gmail.com>
2024-07-25 22:43:15 +0530

    Tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 // import React from "react";
 import { AllRoutes } from "./routes/AllRoutes";
+// import "./index.css";
+
 export const App = () => {
   return (
     <div>
       <AllRoutes />
+      <h1 className="border-lime-400	">new class</h1>
     </div>
   );
 };
--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,7 +1,7 @@
 export const MovieList = () => {
   return (
     <main>
-      <div className="text-9xl">MovieList</div>
+      <div className="">MovieList</div>
     </main>
   );
 };
